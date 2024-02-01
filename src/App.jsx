b7bb63e1e8525c181beea94534df77d1023a9486
Ellenn2301 <ellenncristina99@gmail.com>
2024-01-31 21:07:53 -0300

    corrigi o import do footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Banner from "./componentes/Banner/banner";
-import Footer from "./componentes/Footer/Footer";
+import Footer from "./componentes/Footer/footer";
 
 function App() {
 
