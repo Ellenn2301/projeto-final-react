f81f2b27f8e637f46c3a77b78d80c828eded940d
Ellenn2301 <ellenncristina99@gmail.com>
2024-02-03 11:34:05 -0300

    Melhorando a responsividade.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,26 +3,31 @@ import './HomePage.css';
 
 const HomePage = () => {
     return (
-        <div>
-            <nav>
+        <>
+            <div>
                 <header className='containerBanner'>
                     <h2 className='tituloBanner'>Projeto Final - Módulo React </h2> 
-                    <img className='meuBanner' src='/imagens/banner.svg' alt='Imagem centralizada em uma estudante fazendo anotações.' />
+                    <img className='meuBanner' src='/imagens/banner.svg' alt='Ilustração de uma mulher negra utilizando uma tela.' />
                 </header>
-            </nav>
+            </div>
 
-            <main >
+            <div>
                 <section className='containerMapaPessoal'>
-                    <div >
+                    <div>
                         <img className='mapaEllen' src='/imagens/ellen.png' alt='Ellen Cristina: uma mulher negra, com cabelos castanhos e blusa cor de lilás'/>
                     </div> 
 
-                    <div className='containerDescricaoPessoal'>
+                    <div className='descricaoPessoal'>
                         <h1 className='nomeDescricao'>Ellen Cristina</h1>
                         <h3 className='profissaoDescricao'>Estudante de Análise e Desenvolvimento de Sistemas</h3>
-                        <p className='descricao'>Como resultado de esforços coletivos, quem eu sou, durante toda a minha vida, esteve e está atravessado pela Educação. Sou formada em Ciências Sociais - licenciatura, profissão que exerço com alegria e luta. 
-                        Atualmente tento conciliar os estudos para ingressar na Pós-Graduação, o meu trabalho como pesquisadora e estou descobrindo a programação: um campo novo, que se antes distante, agora faz parte do meu cotidiano, a partir do curso da PretaLab.
-                        Estou iniciando no curso de ADS pelo Instituto Federal de Pernambuco. </p>
+                        
+                        <article>
+                        <p className='descricao'>
+                            Como resultado de esforços coletivos, quem eu sou, durante toda a minha vida, esteve e está atravessado pela Educação. Sou formada em Ciências Sociais - licenciatura, profissão que exerço com alegria e luta. 
+                            Atualmente tento conciliar os estudos para ingressar na Pós-Graduação, o meu trabalho como pesquisadora e estou descobrindo a programação: um campo novo, que se antes distante, agora faz parte do meu cotidiano, a partir do curso da PretaLab.
+                            Estou iniciando no curso de ADS pelo Instituto Federal de Pernambuco. 
+                        </p>
+                        </article>   
                     </div>
                 </section> 
 
@@ -37,9 +42,8 @@ const HomePage = () => {
                         <img src='imagens/react.png' className='imgSkill'/>
                     </div>
                 </section>
-            </main>
-        </div>
-        
+            </div>
+            </>
     )
     
 };
